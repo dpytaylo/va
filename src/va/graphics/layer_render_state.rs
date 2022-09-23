abd263b7cf215b4cebfd24200577c2b5fc6b48e4
dpytaylo <dpytaylo@gmail.com>
2022-09-23 23:05:42 +0200

    update

---
+++ src/va/graphics/layer_render_state.rs
@@ -0,0 +1,127 @@
+use std::{rc::Rc, cell::Ref, sync::Arc};
+
+use anyhow::Context;
+use bytemuck::{Zeroable, Pod};
+use ttf_parser::gpos::VariationDevice;
+use vulkano::{buffer::CpuAccessibleBuffer, render_pass::{Framebuffer, Subpass, RenderPass}, pipeline::{graphics::{viewport::{Viewport, ViewportState}, color_blend::ColorBlendState, input_assembly::InputAssemblyState, vertex_input::BuffersDefinition}, GraphicsPipeline, Pipeline}, command_buffer::{PrimaryAutoCommandBuffer, RenderPassBeginInfo}, descriptor_set::{PersistentDescriptorSet, WriteDescriptorSet}, image::{ImmutableImage, ImageViewAbstract, view::ImageView}, shader::ShaderModule, sampler::{Sampler, SamplerCreateInfo}, device::Device};
+
+use crate::{utils::math::vector::vector2::Vec2, manager::Manager};
+
+use super::{render_state::RenderState, font::Font, Graphics, window_render::WindowRender};
+
+struct LabelRenderState {
+    image_view: Arc<ImageView<ImmutableImage>>,
+}
+
+#[repr(C)]
+#[derive(Default, Debug, Clone, Copy, Zeroable, Pod)]
+pub struct LabelRenderStateVertex {
+    position: [f32; 2],
+    tex_coords: [f32; 2],
+}
+vulkano::impl_vertex!(LabelRenderStateVertex, position, tex_coords);
+
+impl LabelRenderState {
+    fn new(manager: &Rc<Manager>, device: Arc<Device>, render_pass: Arc<RenderPass>, image_view: Arc<ImageView<ImmutableImage>>) 
+        -> anyhow::Result<Rc<Self>>
+    {
+        let graphics_pipeline = manager.load_graphics_pipeline("font_render_state", 
+            |device, manager| 
+        {
+            let vs = manager
+                .load_shader("shaders/spv/texture2d/vert.spv")?;
+            let fs = manager
+                .load_shader("shaders/spv/texture2d/frag.spv")?;
+            
+            let subpass = Subpass::from(render_pass, 0).unwrap();
+            let graphics_pipeline = GraphicsPipeline::start()
+                .vertex_input_state(
+                    BuffersDefinition::new()
+                        .vertex::<LabelRenderStateVertex>(),
+                )
+                .vertex_shader(
+                    vs
+                        .entry_point("main")
+                        .expect("no shader entry point"),
+                    (),
+                )
+                .input_assembly_state(InputAssemblyState::new())
+                .viewport_state(ViewportState::viewport_dynamic_scissor_irrelevant())
+                .fragment_shader(
+                    fs
+                        .entry_point("main")
+                        .expect("no shader entry point"),
+                    (),
+                )
+                .color_blend_state(ColorBlendState::new(subpass.num_color_attachments()).blend_alpha())
+                .render_pass(subpass)
+                .build(device)?; // TODO: build_with_cache ?
+
+            Ok(graphics_pipeline)
+        })?;
+            
+        let layout = Arc::clone(
+            graphics_pipeline
+                .layout()
+                .set_layouts()
+                .get(0)
+                .context("invalid descriptor set layout")?,
+        );
+
+        let sampler = Sampler::new(
+            device,
+            SamplerCreateInfo::simple_repeat_linear(),
+        )?;
+
+        let descriptor_set = PersistentDescriptorSet::new(
+            layout,
+            vec![WriteDescriptorSet::image_view_sampler(0, image_view, sampler)].into_iter()
+        )?;
+
+        Ok(Rc::new(Self {
+            image_view,
+        }))
+    }
+}
+
+impl RenderState<Vec2<f32>> for LabelRenderState {
+    fn command_buffer(
+        &self,
+        graphics: &Rc<Graphics>,
+        buffer: Ref<Arc<CpuAccessibleBuffer<[Vec2<f32>]>>>,
+        framebuffer: Arc<Framebuffer>,
+        viewport: Viewport,
+    ) -> anyhow::Result<PrimaryAutoCommandBuffer> 
+    {
+        let device = graphics.device().expect("no available device");
+        let queue = graphics.queue().expect("no available queue");
+
+        let render_pass_begin_info = RenderPassBeginInfo {
+            clear_values: vec![Some(ClearValue::Float([0.0, 0.0, 0.0, 1.0]))],
+            ..RenderPassBeginInfo::framebuffer(framebuffer)
+        };
+
+        let mut builder = AutoCommandBufferBuilder::primary(
+            device,
+            queue.family(),
+            CommandBufferUsage::SimultaneousUse,
+        )?;
+        builder
+            .begin_render_pass(render_pass_begin_info, SubpassContents::Inline)?
+            .set_viewport(0, [viewport])
+            .bind_pipeline_graphics(Arc::clone(&self.graphics_pipeline))
+            //.push_constants(layout, 0, constants)
+            //.push_descriptor_set(PipelineBindPoint::Graphics, layout, 0, descriptor_set)
+            .bind_descriptor_sets(
+                PipelineBindPoint::Graphics,
+                Arc::clone(self.graphics_pipeline.layout()),
+                0,
+                Arc::clone(&self.descriptor_set),
+            )
+            .bind_vertex_buffers(0, Arc::clone(&buffer))
+            .draw(buffer.len() as u32, 1, 0, 0)?
+            .end_render_pass()?;
+
+        Ok(builder.build()?)
+    }
+}
